f154a32361a35b1a323440fce74471f346e901f0
Travis Smith <Travis.Smith2@gmail.com>
2018-07-16 14:45:39 -0400

    added mongoose schema and routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-const path = require("path");
-const router = require("express").Router();
+const path = require('path');
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
-// API Routes
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("client/build"));
+app.use(express.static("public"));
 app.use(routes);
 
 mongoose.Promise = global.Promise;
